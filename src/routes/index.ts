ac7084b37b6c229280173fe5393e5774777d7703
Raul Max <raulmax319@gmail.com>
2021-05-06 21:22:41 -0300

    ⚡update: dinamically setting up routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,14 @@
-export class Router {}
+import * as controllers from '~/controllers';
+
+export default () => {
+  const controllerInstances = [];
+
+  for (const name of Object.keys(controllers)) {
+    const controller = (controllers as any)[name];
+
+    if (typeof controller === 'function') {
+      controllerInstances.push(new controller());
+    }
+  }
+  return controllerInstances;
+};
